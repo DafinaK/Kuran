c44646d465c4a71ece31620a058d24ac228e2a12
DafinaK <122215192+DafinaK@users.noreply.github.com>
2024-05-24 17:57:33 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,3 @@
+let mostbeautiful_surah_left=document.getElementById('mostbeautiful_surah_left');
+let mostbeautiful_surah_right=document.getElementById('mostbeautiful_surah_right');
+let mostbeautiful_surah=document.getElementsByClassName('mostbeautiful_surah')[0];
